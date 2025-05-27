7ac582f4dda04cc3230a6f51d4780ed376d7b63a
Tonyelchediac <130777962+Tonyelchediac@users.noreply.github.com>
2025-05-27 11:59:18 +0300

    Add files via upload

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,30 +1,33 @@
-// firebase-messaging-sw.js
-importScripts(
-  "https://www.gstatic.com/firebasejs/9.6.10/firebase-app-compat.js"
-);
-importScripts(
-  "https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging-compat.js"
-);
+// main.js or firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getMessaging, getToken, onMessage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging.js";
 
-firebase.initializeApp({
+const firebaseApp = initializeApp({
   apiKey: "AIzaSyD7I_8AE2hWRUUkDlwNOTHlxeajEjsS6-Q",
   authDomain: "fir-pushnotifications2025.firebaseapp.com",
   projectId: "fir-pushnotifications2025",
   storageBucket: "fir-pushnotifications2025.firebasestorage.app",
   messagingSenderId: "465763707770",
   appId: "1:465763707770:web:831a742ac14fc3beb684c3",
-  measurementId: "G-91PTLH96E5",
 });
 
-const messaging = firebase.messaging();
+const messaging = getMessaging(firebaseApp);
 
-messaging.onBackgroundMessage(function (payload) {
-  console.log("Received background message ", payload);
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: payload.notification.icon,
-  };
+navigator.serviceWorker.register('/firebase-messaging-sw.js')
+  .then((registration) => {
+    console.log('Service Worker registered:', registration);
 
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
+    getToken(messaging, {
+      vapidKey: "BNqp2cczO1500ff9J8J8TuRLUctYSYwWEEkX3lp2e9BjZsPoLEgTk6HjvMKqrZH_p9rUHdmnYgv-Ozs2xas8Mf4",
+      serviceWorkerRegistration: registration,
+    }).then((currentToken) => {
+      if (currentToken) {
+        console.log('Token received:', currentToken);
+        // Send this token to your server or use it in FCM send
+      } else {
+        console.log('No registration token available. Request permission to generate one.');
+      }
+    }).catch((err) => {
+      console.error('An error occurred while retrieving token. ', err);
+    });
+  });
